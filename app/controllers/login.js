812079c970d8ac36346cd1c9575f6c25298dff15
Alex Tiley <alextiley@gmail.com>
2013-11-03 20:14:09 +0000

    redirect to login page when /admin is requested

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,7 +1,12 @@
 
 module.exports.controller = function () {
 
-	app.get(/^\/admin(\/login)?\/?$/, function (request, response) {
+	// @todo: auth, then redirect to login / dashboard
+	app.get(/^\/admin(\/)?$/, function (request, response) {
+		response.redirect('/admin/login');
+	});
+
+	app.get(/^\/admin\/login\/?$/, function (request, response) {
 		response.render('login');
 	});
 
